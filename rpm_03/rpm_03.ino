c70edad28a918285256d93d1209d45deca94559d
Adam Baumgartner <adambaumgartner@gmail.com>
2026-03-04 23:44:30 -0500

    arduino version working

--- rpm_03/rpm_03.ino
+++ rpm_03/rpm_03.ino
@@ -2,28 +2,58 @@
  * @brief Ardunio version of rpm display using Round Robin with Interrupts
  */
 
-volatile bool flagRisingEdge = false;
+#define MSTOMIN 60000
 
-volatile bool flagPrintRPM = false;
+volatile bool _risingEdge = false;
+volatile 
 
-int count = 0;
+uint16_t printInterval = 1000;
 
-void (){
-  flag = true;
+int16_t count = 0;
+uint32_t now = 0;
+uint32_t last = 0;
+uint32_t timestamps[8] = {0};
+
+void ISR_Handler(){
+  _risingEdge = true;
 }
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Booting...");
+  Serial.println("RPM, Count");
+
+  // Configure Hardware Interrupt
   pinMode(2, INPUT);
-  attachInterrupt(digitalPinToInterrupt(2), ISR_handler, RISING);
+  attachInterrupt(digitalPinToInterrupt(2), ISR_Handler, RISING);
 }
 
 void loop() {
+  now = millis();
 
-  if(flagRisingEdge) {
-    flagRisingEdge = false;
-    Serial.println(count++);
+  if(_risingEdge) {
+    _risingEdge = false;
+    timestamps[count % 8] = now;
+    count++;
   }
-  
+
+  if(now - last >= printInterval){
+    last = now;
+
+    if(count < 8) {
+      rpm = 0;
+    } else {
+      int16_t deltaMS = timestamps[(count-1) % 8] - timestamps[count % 8];
+      int16_t rpm = (count * MSTOMIN) / (4 * deltaMS);
+    }
+
+    Serial.print(rpm);
+    Serial.print(", ");
+    Serial.println(count);
+
+    for(int i = 0; i <=8; i++){
+      timestamps[i] = 0;
+    }
+
+  }
+
 }
